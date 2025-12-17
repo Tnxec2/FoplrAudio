b73bc0d30a6bfb3fadb4b35c3bceb1fab3f9f6a9
tnxec2 <tnxec2@gmail.com>
2025-12-17 11:13:38 +0100

    rebuild

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0.1"
+        versionName = "1.0.2"
         setProperty("archivesBaseName", "$applicationId-v$versionCode($versionName)")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
